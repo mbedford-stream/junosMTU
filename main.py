e3042cca3edce9461b340ad4f53fde7a27ec3ae0
Mark Bedford <mbedford@streamnetworksinc.com>
2021-08-12 19:20:27 +0000

    generate sets

--- main.py
+++ main.py
@@ -7,13 +7,21 @@ if __name__ == "__main__":
     for h in hosts:
         print("\n\nChecking interfaces on %s\n=================================\n" % h)
         intsList = getInts({"host": h, "port": 830, "user": "mark",
-                            "password": "darkS!de"})
+                            "password": "password123"})
+
+        updateInts = []
 
         for i in intsList:
 
             if int(i.mtu) <= 6000:
                 print("%s\n%s Current MTU: %d\n" %
                       (i.description, i.name, i.mtu))
+
+                updateInts.append(i.name)
+
             else:
                 print("%s\n%s Current MTU OK: %d\n" %
                       (i.description, i.name, i.mtu))
+
+        for u in updateInts:
+            print("set interfaces %s mtu 9192" % u)
